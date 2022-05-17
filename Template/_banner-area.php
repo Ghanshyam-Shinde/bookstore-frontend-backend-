79ec948ad553da395020353d0b48ebff454e27af
Ghanshyam Shinde <shindeghanshyam1207@gmail.com>
2022-05-18 01:59:55 +0530

    initial commit

---
+++ Template/_banner-area.php
@@ -0,0 +1,10 @@
+<!-- Owl-carousel -->
+<section id="banner-area">
+    <div class="owl-carousel owl-theme">
+        <div class="item">
+           <a herf=#> <img src="assets/products/header1banner.gif" alt="Banner1" ></a>
+        </div>
+
+    </div>
+</section>
+<!-- !Owl-carousel -->
---
+++ Template/_blogs.php
@@ -0,0 +1,37 @@
+<!-- Blogs -->
+<div id="blog-">
+<section id="blogs">
+    <div class="container py-4">
+        <h4 class="font-rubik font-size-20"><center>Latest Blogs</center></h4>
+        <hr>
+
+        <div class="owl-carousel owl-theme">
+            <div class="item">
+                <div class="card border-0 font-rale mr-5" style="width: 18rem;">
+                    <h5 class="card-title font-size-16">Earth Day</h5>
+                    <img id="blogimg" src="assets/products/bookblog1.jpg" alt="cart image" class="card-img-top">
+                    <p class="card-text font-size-14 text-black-50 py-1">April 22 is Earth Day, and what better way to celebrate than by lacing up your sneakers and getting outside, or at least reading about that. (If your not a fan of “hiking,” I have got a secret for you: Its really just walking with a fancier name.) These books will inspire you to embrace the great outdoors, whether you leave your house or not.</p>
+                    <a href="https://www.amazon.com/amazonbookreview/read/B09Y2C8ZRY?ref_=abr_widget_nav&theme=light" class="color-second text-left">Read more...</a>
+                </div>
+            </div>
+            <div class="item">
+                <div class="card border-0 font-rale mr-5" style="width: 18rem;">
+                    <h5 class="card-title font-size-16">Best science fiction</h5>
+                    <img id="blogimg" src="assets/products/bookblog2.jpg" alt="cart image" class="card-img-top">
+                    <p class="card-text font-size-14 text-black-50 py-1">A surprising novel from a best-selling author, a “soon-to-be classic” from a writer better known for her musical offerings, and three novels which are sequels to highly acclaimed—and in one case, awards-nominated—series starters. Below is merely a small selection of the Best Science Fiction and Fantasy reads of the month.</p>
+                    <a href="https://www.amazon.com/amazonbookreview/read/B09XL61RW1?ref_=abr_widget_nav&theme=light" class="color-second text-left">Read more...</a>
+                </div>
+            </div>
+            <div class="item">
+                <div class="card border-0 font-rale mr-5" style="width: 18rem;">
+                    <h5 class="card-title font-size-16">Book by Book: John Sandford</h5>
+                    <img id="blogimg" src="assets/products/bookblog3.jpg" alt="cart image" class="card-img-top">
+                    <p class="card-text font-size-14 text-black-50 py-1">The name Davenport will be very familiar to fans of John Sandfords Prey novels. His new series debut, The Investigator, introduces us to Letty Davenport, Lucas adopted daughter, who is: “smart, hard-nosed and hard-bodied, lean, muscled like a dancer, and occasionally displayed a sharp, dry wit.” </p>
+                    <a href="https://www.amazon.com/amazonbookreview/read/B09XLZ7JMS?ref_=abr_widget_nav&theme=light" class="color-second text-left">Read more...</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+</div>
+<!-- !Blogs -->
